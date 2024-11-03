f5401764b4596e01423292acfcef7b74074d23d5
nicaaaaaaa <93868117+nicaaaaaaa@users.noreply.github.com>
2024-11-03 13:00:18 +0800

    Update studentdrugaddiction.py

--- studentdrugaddiction.py
+++ studentdrugaddiction.py
@@ -3,10 +3,7 @@
 import pandas as pd
 import numpy as np
 
-
 import streamlit as st
-
-drive.mount('/content/drive')
 
 data1 = pd.read_csv('https://raw.githubusercontent.com/nicaaaaaaa/jie43203/refs/heads/main/student_addiction_dataset_test.csv')
 data1.head()
