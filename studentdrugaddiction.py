a39f038b7392a58eea8d76e0d1ca52580f731c91
nicaaaaaaa <93868117+nicaaaaaaa@users.noreply.github.com>
2024-11-03 13:12:59 +0800

    Update studentdrugaddiction.py

--- studentdrugaddiction.py
+++ studentdrugaddiction.py
@@ -1,7 +1,27 @@
 # import neccessary libraries
-
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error, mean_absolute_error,  r2_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from google.colab import drive
 import pandas as pd
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay, classification_report
+from sklearn.model_selection import RandomizedSearchCV
+from scipy.stats import randint
+from sklearn.ensemble import RandomForestClassifier
 import numpy as np
+from sklearn.tree import plot_tree
+from sklearn.model_selection import cross_val_score
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
+from six import StringIO
+from IPython.display import Image
+from sklearn.tree import export_graphviz
+import pydotplus
 
 import streamlit as st
 
