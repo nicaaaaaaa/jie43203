d8cd1e0f8fd9a16d78713a6b6af369c4dd6afa99
nicaaaaaaa <93868117+nicaaaaaaa@users.noreply.github.com>
2024-11-04 20:35:44 +0800

    Update Decision_Tree.py

--- pages/Decision_Tree.py
+++ pages/Decision_Tree.py
@@ -84,3 +84,51 @@ df.describe()
 sns.countplot(x='Addiction_Class', hue='Addiction_Class', data=df, palette='Set2')
 plt.title("Target Variable vs Count")
 plt.legend(labels=['No Addict', 'Addict'], title='Addiction Class')
+
+# Load libraries
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
+#splitting data into train and test
+X = df.drop(columns = ['Addiction_Class', 'id', 'Financial_Issues',
+                       'Academic_Performance_Decline', 'Withdrawal_Symptoms', 'Experimentation'])
+
+y = df['Addiction_Class']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=0)
+
+dt = DecisionTreeClassifier(criterion="gini", max_depth=3)
+dt = dt.fit(X_train,y_train)
+y_pred_dt = dt.predict(X_test)
+
+accuracy_dt = accuracy_score(y_test, y_pred_dt)
+print("Accuracy:",accuracy_dt)
+
+from six import StringIO
+from IPython.display import Image
+from sklearn.tree import export_graphviz
+import pydotplus
+
+feature_cols = X.columns
+
+
+dot_data = StringIO()
+export_graphviz(dt, out_file=dot_data,
+                filled=True, rounded=True,
+                special_characters=True, feature_names = feature_cols, class_names=['No addict','Addict'])
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+graph.write_png('diabetes.png')
+Image(graph.create_png())
+
+# intepret the confusion matrix
+#plt.figure(figsize=(8, 6))
+conf_matrix_dt = confusion_matrix(y_test, y_pred_dt)
+sns.heatmap(conf_matrix_dt, annot=True, fmt='d', cmap='Blues', xticklabels=df['Addiction_Class'].unique(), yticklabels=df['Addiction_Class'].unique())
+plt.xlabel('Predicted Label')
+plt.ylabel('True Label')
+plt.title('Confusion Matrix Decision Tree')
+plt.show()
+
+st.pyplot(fig)
